4b0646de728bb174664ea491b00c9f334c72abed
Jorge <coke200898@gmail.com>
2024-02-26 20:54:16 -0300

    agrega state

--- CIRD/MERN/MERN_apoyalo/src/App.jsx
+++ CIRD/MERN/MERN_apoyalo/src/App.jsx
@@ -1,40 +1,53 @@
 // App.jsx
-import React, { useState } from 'react';
-import PersonCard from './components/PersonCard'; // Asegúrate de que la ruta sea correcta
+import React, { useState } from "react";
+import PersonCard from "./components/PersonCard";
 
-import './App.css';
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [favoritos, setFavoritos] = useState([]);
 
   return (
     <>
       <div>
-        {/* Usar el componente PersonCard cuatro veces con diferentes propiedades */}
         <PersonCard
           firstName="John"
           lastName="Doe"
           age={30}
           hairColor="Brown"
+          favoritos={favoritos}
+          setFavoritos={setFavoritos}
         />
         <PersonCard
           firstName="Jane"
           lastName="Smith"
           age={25}
           hairColor="Blonde"
+          favoritos={favoritos}
+          setFavoritos={setFavoritos}
         />
         <PersonCard
           firstName="Bob"
           lastName="Johnson"
           age={40}
           hairColor="Black"
+          favoritos={favoritos}
+          setFavoritos={setFavoritos}
         />
         <PersonCard
-          firstName="Alice"
+          firstName="Mike"
           lastName="Williams"
           age={35}
           hairColor="Red"
+          favoritos={favoritos}
+          setFavoritos={setFavoritos}
         />
+        <h1>Favoritos</h1>
+        <ul>
+          {favoritos.map((nombre, index) => (
+            <li key={index}>{nombre}</li>
+          ))}
+        </ul>
       </div>
     </>
   );
--- CIRD/MERN/MERN_apoyalo/src/components/PersonCard.jsx
+++ CIRD/MERN/MERN_apoyalo/src/components/PersonCard.jsx
@@ -1,18 +1,37 @@
-import React from 'react';
-
-import './PersonCard.css';
+import React from "react";
+import "./PersonCard.css";
+import { useState } from "react";
 
 const PersonCard = (props) => {
+  const [bgColor, setBgColor] = useState("blue");
 
   const personCardStyle = {
-    backgroundColor: "blue",
+    backgroundColor: bgColor,
   };
 
+
+  const favoritosHandler = () => {
+    setBgColor("red");
+    console.log("cambia a red");
+    props.setFavoritos([...props.favoritos, props.firstName]);
+  }
+
+
+  const buttonStyle = { margin: "10px" };
+
   return (
-    <div style={personCardStyle} >
-      <h2>{props.firstName} {props.lastName}</h2>
+    <div style={personCardStyle}>
+      <h2>
+        {props.firstName} {props.lastName}
+      </h2>
       <p>Age: {props.age}</p>
       <p>Hair Color: {props.hairColor}</p>
+      <button
+        style={buttonStyle}
+        onClick={favoritosHandler}
+      >
+        Agregar a favoritos
+      </button>
     </div>
   );
 };
